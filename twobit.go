a18ad8849def66d7936fdd4d319c0b1eca31f910
Andrew E. Bruno <aeb@qnot.org>
2015-04-01 22:39:23 -0400

    Update license

--- twobit.go
+++ twobit.go
@@ -1,5 +1,5 @@
 // Copyright 2015 Andrew E. Bruno. All rights reserved.
-// Use of this source code is governed by a MIT style
+// Use of this source code is governed by a BSD style
 // license that can be found in the LICENSE file.
 
 package twobit
